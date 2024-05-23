5df74c3a07a0653c5b53037e7cf720d481754160
anishsreenivas82 <77966360+anishsreenivas82@users.noreply.github.com>
2024-05-24 05:07:36 +0530

    fix: UI fixes

--- app/src/main/java/com/example/contextualcards/fragments/Hc9ViewHolder.java
+++ app/src/main/java/com/example/contextualcards/fragments/Hc9ViewHolder.java
@@ -19,6 +19,14 @@ public class Hc9ViewHolder extends RecyclerView.ViewHolder{
     }
     public View bind(Card card) {
         // Bind background
+        if(card.getBgImage()!=null){
+            float aspectRatio = card.getBgImage().getAspectRatio();
+            if(aspectRatio!=0){
+                cardContainer.getLayoutParams().width = (int) (cardContainer.getLayoutParams().height * aspectRatio);
+                cardContainer.setLayoutParams(cardContainer.getLayoutParams());
+            }
+
+        }
         BackgroundUtils.setBackground(cardContainer, card.getBgColor(), card.getBgGradient(), card.getUrl());
         cardContainer.setOnClickListener(v->{
             if(card.getUrl()!=null)
